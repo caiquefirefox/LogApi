e963b81e965c00112eb4818edc30ce4fc00184fa
Caique S. Soares <caiqueoucaique@hotmail.com>
2023-02-06 09:42:15 -0300

    Adicionando serilog ao projeto e mudando a arquitetura de infra.

--- src/Log.API/Program.cs
+++ src/Log.API/Program.cs
@@ -1,9 +1,22 @@
 using Log.API.IoC;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
+//builder.Logging.AddConsole();
+//builder.Logging.AddDebug();
+// Use Serilog
+builder.Host.UseSerilog((hostContext, services, configuration) => {
+    configuration
+        .WriteTo.InfluxDB
+        (
+            applicationName: "Quick test",
+            uri: new Uri("http://127.0.0.1:8086"),
+            organizationId: "7bdc1205ae4f3259",  // Organization Id - unique id can be found under Profile > About > Common Ids)
+            bucketName: "logapi",
+            token: "9859DAA6-3B3F-48FE-A981-AE9D31FBB334"
+        );
+});
 
 // Add services to the container.
 
@@ -17,8 +30,10 @@ var app = builder.Build();
 app.UseRouting();
 app.UseLogApi();
 
+Serilog.Log.Information("Adding Routes");
 app.Logger.LogInformation("Adding Routes");
 app.MapControllers();
 
-app.Logger.LogInformation("Starting the app");
+Serilog.Log.Information("Run app");
+app.Logger.LogInformation("Run app");
 app.Run();
